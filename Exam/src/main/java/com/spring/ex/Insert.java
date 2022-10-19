c0f4568122c9251316d33cf978842362c69e54a1
rkdghl2 <rkdghl346@naver.com>
2022-10-19 16:20:53 +0900

    kim

---
+++ Exam/src/main/java/com/spring/ex/Insert.java
@@ -0,0 +1,8 @@
+package com.spring.ex;
+
+public class Insert {
+// Insert 하하하 
+// Insert 하하하 
+// Insert kim
+// Insert 하하하 
+}
---
+++ Exam/src/main/java/com/spring/ex/Update.java
@@ -0,0 +1,7 @@
+package com.spring.ex;
+
+public class Update {
+//	Update r가나다
+//	Update kim
+//	Update r가나다
+}
